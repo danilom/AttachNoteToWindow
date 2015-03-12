4c6c2a60bbcd646ad5e7f3208bb69f8d8b6665d9
danilom <danilom+dev@gmail.com>
2015-03-13 03:33:26 +0700

    Initial commit

---
+++ AttachNoteToWindow/MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace LineConnector
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbLineName = new System.Windows.Forms.TextBox();
+            this.tbNote = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbTopMost = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Window";
+            // 
+            // tbLineName
+            // 
+            this.tbLineName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbLineName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLineName.Location = new System.Drawing.Point(68, 5);
+            this.tbLineName.Margin = new System.Windows.Forms.Padding(5);
+            this.tbLineName.Name = "tbLineName";
+            this.tbLineName.ReadOnly = true;
+            this.tbLineName.Size = new System.Drawing.Size(319, 31);
+            this.tbLineName.TabIndex = 1;
+            // 
+            // tbNote
+            // 
+            this.tbNote.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbNote.Location = new System.Drawing.Point(7, 43);
+            this.tbNote.Margin = new System.Windows.Forms.Padding(2);
+            this.tbNote.Name = "tbNote";
+            this.tbNote.Size = new System.Drawing.Size(380, 189);
+            this.tbNote.TabIndex = 2;
+            this.tbNote.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(7, 239);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbTopMost
+            // 
+            this.cbTopMost.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cbTopMost.AutoSize = true;
+            this.cbTopMost.Location = new System.Drawing.Point(110, 241);
+            this.cbTopMost.Name = "cbTopMost";
+            this.cbTopMost.Size = new System.Drawing.Size(90, 24);
+            this.cbTopMost.TabIndex = 4;
+            this.cbTopMost.Text = "Topmost";
+            this.cbTopMost.UseVisualStyleBackColor = true;
+            this.cbTopMost.CheckedChanged += new System.EventHandler(this.cbTopMost_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 274);
+            this.Controls.Add(this.cbTopMost);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbNote);
+            this.Controls.Add(this.tbLineName);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Attach Notes To Windows";
+            this.Activated += new System.EventHandler(this.MainForm_Activated);
+            this.Deactivate += new System.EventHandler(this.MainForm_Deactivate);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbLineName;
+        private System.Windows.Forms.RichTextBox tbNote;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox cbTopMost;
+    }
+}
+
---
+++ AttachNoteToWindow/MainForm.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using System.Diagnostics;
+
+namespace LineConnector
+{
+    public partial class MainForm : Form
+    {
+        string _windowTitle;
+        Dictionary<string, string> _notes = new Dictionary<string,string>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Load notes
+            LoadNotes();
+        }
+
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            var appName = GetTopWindowName();
+            if (appName.StartsWith(MyProcessName))
+            {
+                return;
+            }
+
+            // check vs an app list
+            var newWindowTitle = appName + ": " + GetTopWindowText();
+            if (newWindowTitle == _windowTitle)
+            {
+                return;
+            }
+
+            // Store old note
+            if (_windowTitle != null)
+            {
+                if (tbNote.Text.Trim() == "")
+                {
+                    _notes.Remove(_windowTitle);
+                }
+                else
+                {
+                    _notes[_windowTitle] = tbNote.Text;
+                }
+                SaveNotes();
+            }
+
+            // Switch
+            _windowTitle = newWindowTitle;
+
+            // Load note
+            tbLineName.Text = _windowTitle;
+            if (_notes.ContainsKey(_windowTitle))
+            {
+                // Note exists
+                tbNote.Text = _notes[_windowTitle];
+                tbNote.BackColor = Color.LightYellow;
+
+                // Show window
+                this.TopMost = true;
+                this.SetOpacity(this.Focused);
+            }
+            else
+            {
+                // Note does not exist
+                tbNote.Text = "";
+                tbNote.BackColor = Color.White;
+
+                this.TopMost = false;
+                this.SetOpacity(this.Focused);
+            }
+        }
+
+        string _myProcessName;
+        string MyProcessName
+        {
+            get
+            {
+                if (_myProcessName == null)
+                {
+                    _myProcessName = Process.GetCurrentProcess().ProcessName;
+                }
+                return _myProcessName;
+            }
+        }
+
+        #region Load/Save data
+        static string Encrypt(string plaintext)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(plaintext);
+            data = data.Select(x => (byte)(x ^ (byte)0x47)).ToArray(); // XOR
+            return Convert.ToBase64String(data);
+        }
+        static string Decrypt(string cryptext)
+        {
+            byte[] data = Convert.FromBase64String(cryptext);
+            data = data.Select(x => (byte)(x ^ (byte)0x47)).ToArray(); // XOR
+            return Encoding.UTF8.GetString(data);
+        }
+
+        void SaveNotes()
+        {
+            var fname = GetNotesFilename();
+            var json = JsonConvert.SerializeObject(this._notes, Formatting.Indented);
+
+            if (fname.EndsWith(".enc"))
+            {
+                json = Encrypt(json);
+            }
+            File.WriteAllText(fname, json);
+        }
+
+        void LoadNotes()
+        {
+            var fname = GetNotesFilename();
+            if (File.Exists(fname))
+            {
+                var json = File.ReadAllText(fname);
+                if (fname.EndsWith(".enc"))
+                {
+                    json = Decrypt(json);
+                }                
+
+                try
+                {
+                    this._notes = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+                    // Special case: empty JSON file
+                    if (this._notes == null)
+                    {
+                        this._notes = new Dictionary<string, string>();
+                    }
+                }
+                catch(JsonReaderException)
+                {
+                    // File was corrupt
+                    MessageBox.Show("File: " + fname + " was corrupted. Will clear all data.");
+                    this._notes = new Dictionary<string, string>();
+                }
+            }
+        }
+
+        string GetNotesFilename()
+        {
+            if (Properties.Settings.Default.FilePath == "")
+            {
+                // Ask
+                if (!SelectNotesFile())
+                {
+                    MessageBox.Show("You must select a file. Exiting.");
+                    Application.Exit();
+                }
+            }
+            return Properties.Settings.Default.FilePath;
+        }
+
+        bool SelectNotesFile()
+        {
+            var fd = new OpenFileDialog();
+            fd.CheckFileExists = false;
+            if (fd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+            {
+                return false;
+            }
+            Properties.Settings.Default.FilePath = fd.FileName;
+            Properties.Settings.Default.Save();
+            return true;
+        }
+        #endregion
+
+        #region Win32
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowTextLength(IntPtr hWnd);
+
+        //  int GetWindowText(
+        //      __in   HWND hWnd,
+        //      __out  LPTSTR lpString,
+        //      __in   int nMaxCount
+        //  );
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        //  DWORD GetWindowThreadProcessId(
+        //      __in   HWND hWnd,
+        //      __out  LPDWORD lpdwProcessId
+        //  );
+        [DllImport("user32.dll")]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        /*
+        //HANDLE WINAPI OpenProcess(
+        //  __in  DWORD dwDesiredAccess,
+        //  __in  BOOL bInheritHandle,
+        //  __in  DWORD dwProcessId
+        //);
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool CloseHandle(IntPtr handle);
+
+        //  DWORD WINAPI GetModuleBaseName(
+        //      __in      HANDLE hProcess,
+        //      __in_opt  HMODULE hModule,
+        //      __out     LPTSTR lpBaseName,
+        //      __in      DWORD nSize
+        //  );
+        [DllImport("psapi.dll")]
+        private static extern uint GetModuleBaseName(IntPtr hWnd, IntPtr hModule, StringBuilder lpFileName, int nSize);
+
+        //  DWORD WINAPI GetModuleFileNameEx(
+        //      __in      HANDLE hProcess,
+        //      __in_opt  HMODULE hModule,
+        //      __out     LPTSTR lpFilename,
+        //      __in      DWORD nSize
+        //  );
+        [DllImport("psapi.dll")]
+        private static extern uint GetModuleFileNameEx(IntPtr hWnd, IntPtr hModule, StringBuilder lpFileName, int nSize);
+        */
+
+        public static string GetTopWindowText()
+        {
+            IntPtr hWnd = GetForegroundWindow();
+            int length = GetWindowTextLength(hWnd);
+            StringBuilder text = new StringBuilder(length + 1);
+            GetWindowText(hWnd, text, text.Capacity);
+            return text.ToString();
+        }
+
+        public static string GetTopWindowName()
+        {
+            IntPtr hWnd = GetForegroundWindow();
+            uint lpdwProcessId;
+            GetWindowThreadProcessId(hWnd, out lpdwProcessId);
+
+            var p = Process.GetProcessById((int)lpdwProcessId);
+            return p.ProcessName;
+        }
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var oldNotesFile = GetNotesFilename();
+            if (SelectNotesFile())
+            {
+                File.Delete(oldNotesFile);
+                SaveNotes();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SaveNotes();
+        }
+
+
+        #region Topmost and opacity
+        void SetOpacity(bool isActive)
+        {
+            this.Opacity = this.TopMost && !isActive ? 0.85 : 1.0;
+        }
+
+        private void cbTopMost_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = cbTopMost.Checked;
+            SetOpacity(true);
+        }
+
+        private void MainForm_Activated(object sender, EventArgs e)
+        {
+            this.SetOpacity(true);
+        }
+
+        private void MainForm_Deactivate(object sender, EventArgs e)
+        {
+            this.SetOpacity(false);
+        }
+        #endregion
+    }
+}
